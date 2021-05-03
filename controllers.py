fac5c459968b503634caf22da670000523612d0f
nickcooperapps <nickcoopersf@gmail.com>
2021-05-02 18:42:34 -0700

    got global posts working

--- controllers.py
+++ controllers.py
@@ -29,12 +29,38 @@ from py4web import action, request, abort, redirect, URL
 from yatl.helpers import A
 from .common import db, session, T, cache, auth, logger, authenticated, unauthenticated, flash
 from py4web.utils.url_signer import URLSigner
-from .models import get_user_email
+from .models import get_user_email, get_time
 
 url_signer = URLSigner(session)
+
+from py4web.utils.form import Form, FormStyleBulma
+from .common import Field
 
 @action('index')
 @action.uses(db, auth, 'index.html')
 def index():
+    rows = db(db.post).select()
     print("User:", get_user_email())
-    return dict()
+    return dict(rows=rows, url_signer=url_signer)
+
+@action('create_post', method=["GET", "POST"]) # the :int means: please convert this to an int.
+@action.uses(db, session, auth.user, 'create_post.html')
+def add():
+    # form = Form(db.post, csrf_session=session, formstyle=FormStyleBulma)
+    # if form.accepted:
+    #     redirect(URL('index'))
+    time = get_time()
+    user_id = auth.current_user.get('id')
+    assert user_id is not None
+    print("user id is", user_id)
+    form = Form([Field('note', 'text'), Field('type')], csrf_session=session,
+                formstyle=FormStyleBulma)
+    if form.accepted:
+        db.post.insert(
+            user_id=user_id,
+            time=time,
+            note=form.vars["note"],
+            type=form.vars["type"]
+        )
+        redirect(URL('index'))
+    return dict(form=form)
--- models.py
+++ models.py
@@ -20,21 +20,25 @@ def get_time():
 #
 ## always commit your models to avoid problems later
 
-db.define_table(
-    'user',
-    Field('first_name', requires=IS_NOT_EMPTY()),
-    Field('last_name', requires=IS_NOT_EMPTY()),
-    Field('user_email', default=get_user_email),
-)
-db.user.user_email.writable = db.user.user_email.readable = False
-db.user.id.readable = db.user.id.writable = False
+# db.define_table(
+#     'user',
+#     Field('first_name', requires=IS_NOT_EMPTY()),
+#     Field('last_name', requires=IS_NOT_EMPTY()),
+#     Field('user_email', default=get_user_email),
+# )
+# db.user.user_email.writable = db.user.user_email.readable = False
+# db.user.id.readable = db.user.id.writable = False
 
 db.define_table(
     'post',
-    Field('user_id', 'reference user'),
-    Field('time', requires=IS_NOT_EMPTY()),
-    Field('note','text', requires=IS_NOT_EMPTY()),
+    # Field('user_id', 'reference user'),
+    Field('user_id', 'reference auth_user'),
+    Field('time', default=get_time),
+    Field('note', 'text', requires=IS_NOT_EMPTY()),
     Field('type', requires=IS_NOT_EMPTY()),
 )
+
+db.post.time.writable = db.post.time.readable = False
+db.post.user_id.writable = db.post.user_id.readable = False
 
 db.commit()
